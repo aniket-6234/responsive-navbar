456dc458191855139cd9df80244df7dc289c01b8
aniket-6234 <aniketashok1999@gmail.com>
2021-11-05 20:13:51 +0530

    navbar logic

--- app.js
+++ app.js
@@ -2,6 +2,41 @@ const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 
+
+//- text animation
+const text = document.querySelector(".fancy");
+const strText = text.textContent;
+const splitText = strText.split("");
+
+//console.log(splitText)
+text.textContent = "";
+for(let i = 0; i < splitText.length; i++) {
+   text.innerHTML += "<span>" + splitText[i] + "</span>";
+}
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick() {
+   const span = text.querySelectorAll('span')[char];
+   span.classList.add('fade');
+   char++;
+   if(char === splitText.length) {
+     complete();
+     return;
+   }
+}
+
+function complete() {
+  clearInterval(timer);
+  timer = null;
+}
+
+
+
+
+//- responsive nav bar logic
+
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle('open');
 })
